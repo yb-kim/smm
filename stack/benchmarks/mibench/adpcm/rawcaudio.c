0d9102512e37e278879e63cb6666b82d1eb62fe9
Jian Cai <jcai19@asu.edu>
2015-07-01 21:14:44 -0700

    Update benchmarks

---
+++ stack/benchmarks/mibench/adpcm/rawcaudio.c
@@ -0,0 +1,42 @@
+/* testc - Test adpcm coder */
+
+#include "adpcm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#ifdef STAT
+extern unsigned long _num_sstore, _num_sload, _num_l2g, _num_g2l, _num_ptr_wr;
+#endif
+
+struct adpcm_state state;
+
+#define NSAMPLES 1000
+
+char	abuf[NSAMPLES/2];
+short	sbuf[NSAMPLES];
+
+int main() {
+    int n;
+
+    while(1) {
+	n = read(0, sbuf, NSAMPLES*2);
+	if ( n < 0 ) {
+	    perror("input file");
+	    exit(1);
+	}
+	if ( n == 0 ) break;
+	adpcm_coder(sbuf, abuf, n/2, &state);
+	write(1, abuf, n/4);
+    }
+    //fprintf(stderr, "Final valprev=%d, index=%d\n",
+	    //state.valprev, state.index);
+    printf("Final valprev=%d, index=%d\n", state.valprev, state.index);
+ 
+#ifdef STAT
+    fprintf(stderr, "_num_dma=%lu, _num_sstore=%lu, _num_sload=%lu, _num_l2g=%lu, _num_g2l=%lu, _num_ptr_wr=%lu\n", _num_sstore + _num_sload + _num_g2l + _num_ptr_wr, _num_sstore, _num_sload, _num_l2g, _num_g2l, _num_ptr_wr);
+#endif
+    //exit(0);
+    return 0;
+}
---
+++ stack/benchmarks/mibench/sha/sha_driver.c
@@ -0,0 +1,38 @@
+/* NIST Secure Hash Algorithm */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "sha.h"
+
+#ifdef STAT
+extern unsigned long _num_sstore, _num_sload, _num_l2g, _num_g2l, _num_ptr_wr;
+#endif
+
+int main(int argc, char **argv)
+{
+    FILE *fin;
+    SHA_INFO sha_info;
+
+    if (argc < 2) {
+	fin = stdin;
+	sha_stream(&sha_info, fin);
+	sha_print(&sha_info);
+    } else {
+	while (--argc) {
+	    fin = fopen(*(++argv), "rb");
+	    if (fin == NULL) {
+		printf("error opening %s for reading\n", *argv);
+	    } else {
+		sha_stream(&sha_info, fin);
+		sha_print(&sha_info);
+		fclose(fin);
+	    }
+	}
+    }
+#ifdef STAT
+    fprintf(stderr, "_num_dma=%lu, _num_sstore=%lu, _num_sload=%lu, _num_l2g=%lu, _num_g2l=%lu, _num_ptr_wr=%lu\n", _num_sstore + _num_sload + _num_g2l + _num_ptr_wr, _num_sstore, _num_sload, _num_l2g, _num_g2l, _num_ptr_wr);
+#endif
+    return(0);
+}
