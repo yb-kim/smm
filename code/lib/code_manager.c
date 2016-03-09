7bb9c5c2b95a482e9a1c155ef247d07e70608c64
Jian Cai <jcai19@asu.edu>
2016-03-09 16:12:51 -0700

    Code Management: fix a bug on string copying in code management library

--- code/lib/code_manager.c
+++ code/lib/code_manager.c
@@ -11,6 +11,8 @@ Map *_mapping_table;
 int mapping_table_size;
 int region_table_size;
 
+//extern long dmaCount;
+
 int c_init_reg(int num) {
     int i;
     //va_list vl;
@@ -59,9 +61,17 @@ char* c_get(char *func_name) {
     }
 
     if(strcmp(func_name, map->reg->func_name)) {
-	//memcpy(map->func_vma, map->func_lma, map->func_size);
+	size_t len = strlen(func_name);
+	//printf("Before DMA, func_name=%s, map->reg->func_name=%s, map->func_vma=%lx, map_func_lma=%lx, dmaCount=%ld\n", func_name, map->reg->func_name, (unsigned long)map->func_vma, (unsigned long)map->func_lma, dmaCount);
+	//++dmaCount;
 	dma(map->func_vma, map->func_lma, map->func_size, MEM2SPM);
+	if (map->reg->func_name) 
+	    free(map->reg->func_name);
+	map->reg->func_name = (char *)malloc(len+1);
 	strcpy(map->reg->func_name, func_name);
+	map->reg->func_name[len] = 0;
+	//printf("After DMA, func_name=%s, map->reg->func_name=%s, map->func_vma=%lx, map_func_lma=%lx, dmaCount=%ld\n", func_name, map->reg->func_name, (unsigned long)map->func_vma, (unsigned long)map->func_lam, dmaCount);
+	//printf("After DMA, func_name=%s, map->reg->func_name=%s, map->func_vma=%lx, map_func_lma=%lx, dmaCount=%ld\n", func_name, map->reg->func_name, (unsigned long)map->func_vma, (unsigned long)map->func_lma, dmaCount);
     }
     return map->func_vma;
 }
